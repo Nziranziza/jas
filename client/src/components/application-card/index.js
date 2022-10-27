84f4e3a6602f31366309542db2722509f1a1f8bd
Nziranziza <nziranzizadaniel@gmail.com>
2022-10-27 10:02:37 +0200

    Add mobile responsiveness

--- client/src/components/application-card/index.js
+++ client/src/components/application-card/index.js
@@ -1,28 +1,23 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import "./style.scss";
 import { applicationStatuses } from '../../constants';
 
 export function ApplicationCard({ id, name, cvUrl, status, onStatusChange = () => {} }) {
   const [_status, setStatus] = useState(status);
-  const navigate = useNavigate();
 
   const onSelect = (e) => {
     setStatus(e.target.value)
     onStatusChange(e.target.value)
   }
 
-  const onClick = () => {
-    navigate(`applications/${id}`)
-  }
-
   return (
-    <div className="application-card" onClick={onClick}>
-      <div className="application-item">
+    <div className="application-card">
+      <Link className="application-item" to={`applications/${id}`}>
         <span className="label">Applicant name:</span>
         <span>{name}</span>
-      </div>
+      </Link>
       <div className="application-item">
         <span className="label">CV:</span>
         <a href={cvUrl} target="_blank" rel="noreferrer">{cvUrl}</a>
